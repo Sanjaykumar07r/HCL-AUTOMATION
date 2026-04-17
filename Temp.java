12cbce99d41ee060a8f1e72c87dbb1c95a5eb5db
Sanjaykumar07r <your-email@gmail.com>
2026-04-17 11:58:49 +0530

    new branch commit

--- Temp.java
+++ Temp.java
@@ -1,3 +1,10 @@
 public class Temp{
-    g
+    public static void main(String[] args) {
+       
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
+        
+    }
 }
